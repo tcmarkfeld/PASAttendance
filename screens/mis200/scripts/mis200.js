e91c5c22316a1896c71deb7a5f40baba1d27ce92
tcmarkfeld <tcmarkfeld@crimson.ua.edu>
2022-03-07 17:43:41 -0600

    Data Table data

--- screens/mis200/scripts/mis200.js
+++ screens/mis200/scripts/mis200.js
@@ -6,57 +6,51 @@ const getUsers = async () => {
   return data;
 };
 
-function populateFields(data) {
-  for (let i = 0; i < data.Length; i++) {
-    var id = document.getElementById("userId");
-    var html = `${data[i].userId}`;
-    id.innerHTML = html;
-    console.log(data[i]);
+const populateFields = async () => {
+  const userURL = `http://10.8.1.20:83/api/mis200users`;
+  const response = await fetch(userURL);
+  const data = await response.json();
 
-    var name = document.getElementById("userFullName");
-    var html = `${data[i].userName}`;
-    name.innerHTML = html;
+  $("#myTable").DataTable({
+    data: data,
+    columns: [
+      { data: "userId" },
+      { data: "userFullName" },
+      { data: "userEmail" },
+      { data: "role" },
+    ],
+  });
 
-    var email = document.getElementById("userEmail");
-    var html = `${data[i].userEmail}`;
-    email.innerHTML = html;
+  //   var html = `<tbody>`;
+  //   for (const user of data) {
+  //     html += '<tr id="tableRow">';
+  //     html += `<td>${user.userId}</td>`;
 
-    var role = document.getElementById("role");
-    var html = `${data[i].role}`;
-    role.innerHTML = html;
-  }
-  //   console.log(data);
-  //   data[0].array.forEach((element) => {
-  //     console.log(element);
-  //     var id = document.getElementById("userId");
-  //     var html = `${element[0].userId}`;
-  //     id.innerHTML = html;
+  //     html += `<td>${user.userFullName}</td>`;
 
-  //     var name = document.getElementById("userFullName");
-  //     var html = `${data[element].userName}`;
-  //     name.innerHTML = html;
+  //     html += `<td>${user.userEmail}</td>`;
 
-  //     var email = document.getElementById("userEmail");
-  //     var html = `${data[element].userEmail}`;
-  //     email.innerHTML = html;
+  //     html += `<td>${user.role}</td>`;
+  //     html += "</tr>";
+  //   }
+  //   html += `</tbody>`;
+  //   document.getElementById("tableBody").innerHTML = html;
 
-  //     var role = document.getElementById("role");
-  //     var html = `${data[element].role}`;
-  //     role.innerHTML = html;
-  //   });
-  // var id = document.getElementById("userId");
-  // var html = `${data[0].userId}`;
-  // id.innerHTML = html;
+  //console.log(html);
+  //   var id = document.getElementById("userId");
+  //   var html = `${data[0].userId}`;
+  //   id.innerHTML = html;
 
-  // var name = document.getElementById("userFullName");
-  // var html = `${data[0].userName}`;
-  // name.innerHTML = html;
+  //   console.log(data[0]);
+  //   var name = document.getElementById("userName");
+  //   var html = `${data[0].userFullName}`;
+  //   name.innerHTML = html;
 
-  // var email = document.getElementById("userEmail");
-  // var html = `${data[0].userEmail}`;
-  // email.innerHTML = html;
+  //   var email = document.getElementById("userEmail");
+  //   var html = `${data[0].userEmail}`;
+  //   email.innerHTML = html;
 
-  // var role = document.getElementById("role");
-  // var html = `${data[0].role}`;
-  // role.innerHTML = html;
-}
+  //   var role = document.getElementById("userRole");
+  //   var html = `${data[0].role}`;
+  //   role.innerHTML = html;
+};
